7448b5973172e1b9add47f4e5673f761d8f745af
Abror Abduganiev <developabror@gmail.com>
2024-06-08 17:36:06 +0500

    telegram bot

--- src/main/java/uz/app/BotService.java
+++ src/main/java/uz/app/BotService.java
@@ -5,42 +5,90 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static uz.app.Utils.*;
 
 public class BotService extends TelegramLongPollingBot {
 
+
+    static Map<Long, Integer> userState = new HashMap<>();
+
+    @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
-//        System.out.println(update.getMessage().getChat().getId());
-//        System.out.println(update.getMessage().getChat().getFirstName());
-
-//        System.out.println(Thread.currentThread().getName());
-//        System.out.println(update.getMessage().getText());
+        Long chatId = update.getMessage().getChatId();
+        userState.putIfAbsent(chatId, 0);
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setChatId(chatId);
 
-        switch (update.getMessage().getText()) {
-            case "/start" -> {
-                sendMessage.setText("Assalamu alaykum. Botga xush kelibsiz");
-                keyboardMaker(sendMessage,Utils.mainMenu);
-            }
-            case "info"->{
-                sendMessage.setText("here is full info");
-                keyboardMaker(sendMessage,Utils.info);
-            }
-            default -> sendMessage.setText(update.getMessage().getText());
-        }
 
-        try {
+        if (update.getMessage().hasContact()) {
+            System.out.println(update.getMessage().getContact().getPhoneNumber());
+            keyboardMaker(sendMessage, info);
+            sendMessage.setText("here is info");
+            userState.put(chatId,1);
             execute(sendMessage);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            return;
         }
-        System.out.println("success");
+
+        Integer state = userState.get(chatId);
+        String text = update.getMessage().getText();
+
+        switch (text) {
+            case "/start" -> {
+                state = 0;
+                sendMessage.setText("Assalamu alaykum. Botga xush kelibsiz");
+                keyboardMaker(sendMessage, Utils.mainMenu);
+            }
+            case Utils.INFO -> {
+                state = 1;
+                sendMessage.setText("here is full info");
+                keyboardMaker(sendMessage, Utils.info);
+            }
+            case MAIN_ASKED -> {
+                state = 2;
+                keyboardMaker(sendMessage, new String[][]{{MAIN_ASKED}, {BACK}});
+            }
+            case FILIALS -> {
+                state = 2;
+                keyboardMaker(sendMessage, new String[][]{{FILIALS}, {BACK}});
+            }
+            case WORKING_DAYS -> {
+                state = 2;
+                keyboardMaker(sendMessage, new String[][]{{WORKING_DAYS}, {BACK}});
+            }
+            case PRODUCTS -> {
+                state = 2;
+                keyboardMaker(sendMessage, new String[][]{{PRODUCTS}, {BACK}});
+            }
+            case WORKING_HOURS -> {
+                state = 2;
+                keyboardMaker(sendMessage, new String[][]{{WORKING_HOURS}, {BACK}});
+            }
+            case BACK -> {
+                if (state == 1) {
+                    state = 0;
+                    keyboardMaker(sendMessage, mainMenu);
+                } else if (state == 2) {
+                    state = 1;
+                    keyboardMaker(sendMessage, info);
+                }
+            }
+            default -> keyboardMaker(sendMessage, mainMenu);
+        }
+        userState.put(chatId, state);
+        if (sendMessage.getText() == null) {
+            sendMessage.setText(text);
+        }
+        execute(sendMessage);
     }
 
     private static void keyboardMaker(SendMessage sendMessage, String[][] buttons) {
@@ -52,12 +100,20 @@ public class BotService extends TelegramLongPollingBot {
         List<KeyboardRow> keyboardRows = new ArrayList<>();
         for (String[] button : buttons) {
             KeyboardRow row = new KeyboardRow();
-            for (String s : button)
-                row.add(s);
+            for (String s : button) {
+                if (s.equals(SEND_NUMBER)) {
+                    KeyboardButton keyboardButton = new KeyboardButton(s);
+                    keyboardButton.setRequestContact(true);
+                    row.add(keyboardButton);
+                } else {
+                    row.add(s);
+                }
+            }
             keyboardRows.add(row);
         }
         replyKeyboardMarkup.setKeyboard(keyboardRows);
     }
+
 
     @Override
     public String getBotUsername() {
--- src/main/java/uz/app/Utils.java
+++ src/main/java/uz/app/Utils.java
@@ -1,13 +1,25 @@
 package uz.app;
 
 public interface Utils {
-    String[][] mainMenu={
-            {"contact us","info"},
-            {"send number"}
+
+    String INFO = "info";
+    String SEND_NUMBER="send number";
+    String[][] mainMenu = {
+            {"contact us", INFO},
+            {SEND_NUMBER}
     };
-    String[][] info={
-            {"filials","products","main asked"},
-            {"working days","working hours"},
-            {"back"}
+
+    String FILIALS = "filials";
+    String PRODUCTS = "products";
+    String MAIN_ASKED = "main asked";
+    String WORKING_DAYS = "working days";
+    String BACK = "back";
+    String WORKING_HOURS = "working hours";
+    String[][] info = {
+            {FILIALS, PRODUCTS, MAIN_ASKED},
+            {WORKING_DAYS, WORKING_HOURS},
+            {BACK}
     };
+
+
 }
